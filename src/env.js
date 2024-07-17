a1db7e244454cc11db537ae5ecd0f008189c4524
Prasuk-Jain-Byldd <prasuk@byldd.com>
2024-07-17 22:24:28 +0530

    fix: env variables for user email verification endpoint

--- src/env.js
+++ src/env.js
@@ -11,6 +11,8 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    JWT_SECRET: z.string().min(20),
+    RESEND_API_KEY: z.string().min(20),
   },
 
   /**
@@ -29,6 +31,8 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    JWT_SECRET: process.env.JWT_SECRET,
+    RESEND_API_KEY: process.env.RESEND_API_KEY
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
