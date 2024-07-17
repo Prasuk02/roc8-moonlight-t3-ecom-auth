8608c40bb8fac059849322718ce56c46c6fa67ba
Prasuk-Jain-Byldd <prasuk@byldd.com>
2024-07-17 10:38:57 +0530

    Add dependencies and fix issues New dependencies related to icons and backend apis (bcryptjs, jwt, resend, tsx, randomatic) fix: Add keyframe to extends in tailwind.config for loader component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,15 @@ export default {
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
       },
+      keyframes: {
+        spin: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
+      animation: {
+        spin: 'spin 1s linear infinite',
+      },
     },
   },
   plugins: [],
