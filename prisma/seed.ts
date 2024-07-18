21ffdb21b64c3cad83a4e18a3e460c68e59914c3
Prasuk-Jain-Byldd <prasuk@byldd.com>
2024-07-18 20:05:48 +0530

    db-improv: Truncate category table before seeding data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,19 +1,24 @@
 
 import { db as prisma } from "../src/server/db";
 import { categories } from "./seed-data/categories";
-// const { categories } = require("./seed-data/categories")
 
+/**
+ * Truncates existing "Category" table and restarts identity cascade before seeding.
+ * Seed categories into the database using Prisma ORM.
+*/
 async function seedCategories() {
-  await prisma.category.createMany({ data: categories })
-}
-
-seedCategories()
-  .then(async () => {
-    console.log('Categories data seeding completed ✅')
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
+  try {
+    await prisma.$queryRawUnsafe(`Truncate "Category" restart identity cascade;`);
+    console.log('✅ Category table truncated successfully.')
+    await prisma.category.createMany({ data: categories });
+    console.log('✅ Category seeded successfully.');
+    await prisma.$disconnect();
+  } 
+  catch (error) {
+    console.error('❌', error)
     await prisma.$disconnect()
     process.exit(1)
-  })
+  }
+}
+
+seedCategories();
